82078dc021d45c08b7af5eaa657c25072075c526
James Nelson <JENkt4k@users.noreply.github.com>
2026-09-04 18:52:01 -0500

    Centralize M7 acceptance inventory

---
+++ scripts/m7_acceptance_inventory.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+ACCEPTANCE_PROJECTS = (
+    "text_pipeline",
+    "binary_packet",
+    "generic_result",
+    "trait_bounds",
+    "generic_collections",
+    "borrowed_views",
+    "bootstrap_lexer",
+    "cobol_finance_modernization",
+    "filesystem_capabilities",
+    "ledger_app",
+)
